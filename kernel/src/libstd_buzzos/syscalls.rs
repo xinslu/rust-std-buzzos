2f97a75a8ddec0dc6095fbcfbf8a0ebbf3cf30bd
Cai, Nicolas <ncai34@gatech.edu>
2023-04-07 11:04:08 -0400

    Cleaned up syscalls

--- kernel/src/libstd_buzzos/syscalls.rs
+++ kernel/src/libstd_buzzos/syscalls.rs
@@ -1,6 +1,6 @@
 use core::{arch::asm, ffi::c_void};
 
-use crate::{interrupts, print, println};
+use crate::{interrupts};
 
 #[derive(Copy, Clone)]
 pub enum Sysno {
@@ -18,11 +18,9 @@ pub unsafe fn syscall0(n: Sysno) -> usize {
 
     match n as u32 {
         // 0x20 => asm!("int 0x20", inlateout("eax") (n as usize) => ret, options(nostack, preserves_flags)),
-        // 0 => interrupts::handlers::sbrk(0),
-        // 1 => interrupts::handlers::read(),
-        // 2 => interrupts::handlers::write(),
         _ => 0,
     };
+    
     ret
 }
 
@@ -35,8 +33,6 @@ pub unsafe fn syscall1(n: Sysno, arg1: usize) -> usize {
     match n as u32 {
         // 0x20 => asm!("int 0x20", inlateout("eax") (n as usize) => ret, in("edx") arg1, options(nostack, preserves_flags)),
         0 => return interrupts::handlers::sbrk() as usize,
-        // 1 => interrupts::handlers::read(),
-        // 2 => return interrupts::handlers::write(),
         _ => 0,
     };
 
@@ -51,12 +47,10 @@ pub unsafe fn syscall2(n: Sysno, arg1: usize, arg2: usize) -> usize {
     asm!("nop", in("ecx") arg1, in("edx") arg2);
     match n as u32 {
         // 0x20 => asm!("int 0x20", inlateout("eax") (n as usize) => ret, in("edx") arg1, options(nostack, preserves_flags)),
-        // 0 => interrupts::handlers::sbrk(),
-        // 1 => interrupts::handlers::read(),
         2 => return interrupts::handlers::write(),
         _ => 0,
     };
-    // println!("{:#?}", arg2);
+
     ret
 }
 
@@ -68,9 +62,7 @@ pub unsafe fn syscall3(n: Sysno, arg1: usize, arg2: usize, arg3: usize) -> usize
     asm!("nop", in("ecx") arg1, in("edx") arg2, in("eax") arg3);
     match n as u32 {
         // 0x20 => asm!("int 0x20", inlateout("eax") (n as usize) => ret, in("edx") arg1, options(nostack, preserves_flags)),
-        // 0 => interrupts::handlers::sbrk(arg1, arg2 as *mut u8),
         1 => interrupts::handlers::read(arg1, arg2 as *mut c_void, arg3),
-        // 2 => interrupts::handlers::write(arg1, arg2 as *mut c_void, arg3),
         _ => 0,
     };
 
